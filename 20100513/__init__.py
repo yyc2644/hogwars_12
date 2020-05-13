542e9e4c52c1b2d7a74d127a6bc5a51921944c64
yicheng <yichengace@hotmail.com>
2020-05-13 22:29:48 +0800

    python 读写文件的操作，读写excel格式的还没学习

---
+++ 20100513/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/13 9:33 下午
+# @Author  : https://github.com/yyc2644
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ 20100513/test_read.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/13 9:34 下午
+# @Author  : https://github.com/yyc2644
+# @File    : test_read.py
+# @Software: PyCharm
