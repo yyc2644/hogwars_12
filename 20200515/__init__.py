f43bd6ea305d1d23f97ca1d042142d4ec63337fc
yicheng <yichengace@hotmail.com>
2020-05-15 23:29:06 +0800

    内置函数的练习

---
+++ 20200515/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/15 11:02 下午
+# @Author  : https://github.com/yyc2644
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ 20200515/normal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/15 11:03 下午
+# @Author  : https://github.com/yyc2644
+# @File    : test_normal.py
+# @Software: PyCharm
+#内置函数的练习
+
+import os
+# os.mkdir('text.txt')
+os.listdir
