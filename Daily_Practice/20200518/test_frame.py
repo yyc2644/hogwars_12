ecaa8be69f6912187e438e277545e0c32afa2973
yicheng <yichengace@hotmail.com>
2020-05-18 23:00:15 +0800

    今天没有敲代码，在配置appium环境

---
+++ Daily_Practice/20200518/test_frame.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/18 12:31 上午
+# @Author  : https://github.com/yyc2644
+# @File    : test_frame.py
+# @Software: PyCharm
---
+++ Daily_Practice/20200518/test_windows.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/18 12:28 上午
+# @Author  : https://github.com/yyc2644
+# @File    : test_windows.py
+# @Software: PyCharm
