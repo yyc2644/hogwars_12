d345f99ec20634a14cb3cc50d3ca56cc8ebaa3da
yicheng <yichengace@hotmail.com>
2020-05-23 23:39:30 +0800

    操作js,没写完，后续再写

---
+++ Daily_Practice/20200523/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/23 11:16 下午
+# @Author  : https://github.com/yyc2644
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Daily_Practice/20200523/test_js.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/23 11:16 下午
+# @Author  : https://github.com/yyc2644
+# @File    : test_js.py
+# @Software: PyCharm
